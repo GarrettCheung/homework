a0f9bf3fd97ee6a73d18da608e1cb2b40a906684
zhangruiname <17601035802@163.com>
2019-02-28 23:37:56 +0800

    第二道题完成！

--- src/demo/Demo1.java
+++ src/demo/Demo1.java
@@ -2,7 +2,7 @@ package demo;
 
 import java.util.Scanner;
 
-public class First {
+public class Demo1 {
     public static void main(String[] args) {
         //古典问题：有一对兔子，从出生后第3个月起每个月都生一对兔子，小兔子长到第三个月后每个月又生一对兔子，假如兔子都不死，问每个月的兔子总数为多少？
         Scanner intput = new Scanner(System.in);
@@ -11,7 +11,7 @@ public class First {
         month = new int[m];
         if( month.length > 0 ){
             for(int i = 0;i<month.length;i++){
-                if(i==0||i==1){
+                if(i==0 || i==1){
                     month[0]=1;month[1]=1;
                 }else {
                     month[i]=month[i-2]+month[i-1];
---
+++ src/demo/Demo2.java
@@ -0,0 +1,24 @@
+package demo;
+
+public class Demo2 {
+    public static void main(String[] args){
+        //判断101-200之间有多少个素数，并输出所有素数。
+        int up = 101,down= 200,count = 0;
+        for(int i=up;i<down;i++){
+            for(int j = 2; j<=i-1;j++){
+                if(i%j==0){
+                    break;
+                }
+                if(j==i-1){
+                    System.out.println("素数为:"+i);
+                }
+
+            }
+            //System.out.println("素数为:"+count);
+        }
+
+
+
+    }
+}
+
